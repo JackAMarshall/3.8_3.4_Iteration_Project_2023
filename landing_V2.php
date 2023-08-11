13a2257b4333d7621fe051cd8c312d8fca7a752a
JackAMarshall <137360370+JackAMarshall@users.noreply.github.com>
2023-08-11 13:06:22 +1200

    Create landing_V2.php

---
+++ landing_V2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!--The meta data-->
+		<title>Graeme's Music</title>
+		<meta charset="UTF-8" />
+		<meta name="description" content="Website for Greames Music" />
+		<meta name="keywords" content="Greame, Music, Wesbite, Jack Marshall" />
+		<meta name="author" content="Jack Marshall" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--The link below links the page to the websites css sheet-->
+		<link rel = "stylesheet" href = "style3.css"/>
+	</head>
+	<body>
+		<img src = "images/background.jpg" alt = "Bacground Image" class = "background_image" />
+		<div class = "wrapper">
+			<div class = "nav">
+				<ul>
+					<li><a href = "login.php">Login</a></li>
+					<li><a href = "create_account.php">Sign Up</a></li>
+				</ul>
+			</div>
+			<div class = "title">
+				<h1>Graeme's Music</h1>
+				<h3>The Greatest Tunes Return</h3>
+			</div>
+			<div class = "content">
+				<h4>Our Mission</h4>
+				<p>Our mission is to display Grames amazing collection of Eclectic music that he has been collected by our sponsor Graeme. This website is meant for public use so any permission is give proactivly for republication, though obviously the quality is questionable as it is the product of a 17 year old learning php and javascrpt for the first time and trying to implement it amongst html and css. So please enjoy. Also why are you ready this, this is just for the balance of the wesbite so it looks like there is actual information on this wesbite. BTW our mission is the destruction of Reggae.</p>
+				<h4>What is Eclectic Music</h4>
+				<p>I don't really know what ecletic music is so here is what wikipedia says: The term can be used to describe the music of composers who combine multiple styles of composition; an example would be a composer using a whole tone scale variant of a folk song in a pentatonic scale over a chromatic counterpoint, or a tertian arpeggiating melody over quartal or secundal harmonies. Eclecticism can also occur through quotations, whether of a style, direct quotations of folk songs/variations of them for example, in Mahler's</p>
+				<h4>Why Ecletic Music is Great</h4>
+				<p>Just becuase, just becuase. Really do I don't know. I wouldn't be able to name a single Eclectic song. I just can't. I heard there's some Beatles musci thats eclectic, but I don't really know. It like hip hop, I can't name any of that either. Put a gun to my head, I just can't. Personally I'm not a huge fan of music in general, so its kinda weird I'm making a music wesbite, but I have to becuase of the assessment criteria. I am actually currently listening to the remix of gollem's song that is based on the ring song for lor of the rings. Its pretty good, but I don't know if its better than gollums song whic is ofcourse a classic in the hollywood adaptation of tolkiens work. I think I've typed enough now. I question why you've read this far.</p>
+			</div>
+			<div class = "footer">
+				<p>Jack Marshall 2023</p>
+			</div>
+		</div>
+	</body>
