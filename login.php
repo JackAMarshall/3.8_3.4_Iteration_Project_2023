18dff25c43570e93a63806ef5a993b535c7eb7e4
JackAMarshall <137360370+JackAMarshall@users.noreply.github.com>
2023-09-07 21:59:27 +1200

    Update login.php

--- login.php
+++ login.php
@@ -1,37 +1,3 @@
-<?php
-	ob_start();
-	session_start();
-	   $error = NULL;
-	   if($_SERVER["REQUEST_METHOD"] == "POST") {
-			//connect.php (tells where to connect servername, dbaseName, username, password)
-			require "assessment_mysqli.php";
-			// username and password sent from form 
-			$myusername = mysqli_real_escape_string($conn,$_POST['username']);
-			$mypassword = mysqli_real_escape_string($conn,$_POST['password']); 
-
-			$query = "SELECT Username FROM user WHERE Username = '$myusername' and Password = '$mypassword'";
-						
-			$result = mysqli_query($conn,$query);
-			$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-		  
-			$count = mysqli_num_rows($result);
-		  
-			// If result matched $myusername and $mypassword, table row must be 1 row
-			if($count == 1 and $myusername == "Graeme"){
-				$_SESSION['login_user'] = $myusername;
-				header("location: admin.php");
-			}
-		   if($count == 1 and $myusername != "Graeme"){
-			   $_SESSION['login_user'] = $myusername;
-				header("location: main.php");
-		   }
-		   else {
-				$error = "ERROR! Your Login Name or Password is invalid";
-				}
-			}
-	ob_end_flush();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -46,7 +12,6 @@
 		<link rel = "stylesheet" href = "style3.css"/>
 	</head>
 	<body>
-		<login>
 		<img src = "images/background.jpg" alt = "Bacground Image" class = "background_image" />
 		<div class = "wrapper">
 			<div class = "nav-fixed">
@@ -54,27 +19,26 @@
 					<h32>Graemes Music</h32>
 					<div class = "nav-buttons">
 						<ul>
-							<li><a href = "landing.php">Home</a></li>
+							<li><a href = "login.php">Login</a></li>
+							<li><a href = "create_account.php">Sign Up</a></li>
 						</ul>
 					</div>
 				</div>
 			</div>
-			<div class = "title_login">
+			<div class = "title">
 				<h1>Graeme's Music</h1>
 				<h3>The Greatest Tunes Return</h3>
-				<p>Please use _ instead of spaces</p>
 			</div>
 			<div class = "content">
-				<form method = "post" id= "login">
-					<input type = "text" name = "username" placeholder="Enter user name"/><br />
-					<input type = "text" name = "password" placeholder="Enter user password"/><br />
-					<input type = "submit" value = " Log In "/><br />
-				</form>
-				<h42 class = "grey"><?php echo $error; ?></h42>
+				<h4>Our Mission</h4>
+				<p>Our mission is to display Grames amazing collection of Eclectic music that he has been collected by our sponsor Graeme. This website is meant for public use so any permission is give proactivly for republication, though obviously the quality is questionable as it is the product of a 17 year old learning php and javascrpt for the first time and trying to implement it amongst html and css. So please enjoy. Also why are you ready this, this is just for the balance of the wesbite so it looks like there is actual information on this wesbite. BTW our mission is the destruction of Reggae.</p>
+				<h4>What is Eclectic Music</h4>
+				<p>I don't really know what ecletic music is so here is what wikipedia says: The term can be used to describe the music of composers who combine multiple styles of composition; an example would be a composer using a whole tone scale variant of a folk song in a pentatonic scale over a chromatic counterpoint, or a tertian arpeggiating melody over quartal or secundal harmonies. Eclecticism can also occur through quotations, whether of a style, direct quotations of folk songs/variations of them for example, in Mahler's</p>
+				<h4>Why Ecletic Music is Great</h4>
+				<p>Just becuase, just becuase. Really do I don't know. I wouldn't be able to name a single Eclectic song. I just can't. I heard there's some Beatles musci thats eclectic, but I don't really know. It like hip hop, I can't name any of that either. Put a gun to my head, I just can't. Personally I'm not a huge fan of music in general, so its kinda weird I'm making a music wesbite, but I have to becuase of the assessment criteria. I am actually currently listening to the remix of gollem's song that is based on the ring song for lor of the rings. Its pretty good, but I don't know if its better than gollums song whic is ofcourse a classic in the hollywood adaptation of tolkiens work. I think I've typed enough now. I question why you've read this far.</p>
 			</div>
 			<div class = "footer">
-				<p>©Jack Marshall 2023</p>
+				<p>Jack Marshall 2023</p>
 			</div>
 		</div>
-		</login>
 	</body>
