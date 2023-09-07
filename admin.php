f1398405efb2a82b1462cff6ca7a39ed58f44279
JackAMarshall <137360370+JackAMarshall@users.noreply.github.com>
2023-09-07 21:38:18 +1200

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!--The meta data-->
+		<title>Greames Music</title>
+		<meta charset="UTF-8" />
+		<meta name="description" content="Website for Greames Music" />
+		<meta name="keywords" content="Greame, Music, Wesbite, Jack Marshall" />
+		<meta name="author" content="Jack Marshall" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--The link below links the page to the websites css sheet-->
+		<link rel = "stylesheet" href = "style3.css"/>
+	</head>
+	<body>
+		<div class = "wrapper">
+			<div class = "grid">
+				<a href = "view_users.php">
+					<button class = "grid-button">
+						<h42>View Users</h42>
+					</button>
+				</a>
+				<a href = "edit_database.php">
+					<button class = "grid-button">
+						<h42>Edit Database</h42>
+					</button>
+				</a>
+			</div>
+			<div class = "footer">
+				<p>Copyright Statement</p>
+			</div>
+		</div>
+	</body>
